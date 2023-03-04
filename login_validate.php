16d7c04c829692a91c1604d854194ec0ee285145
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-03-04 18:11:14 -0300

    add login validate

--- login_validate.php
+++ login_validate.php
@@ -4,28 +4,13 @@ session_start();
 print_r($_SESSION);
 echo "<hr/>";
 
-$usuario_autenticado = false;
-
 $usuarios_app = array(
     array('email' => 'admteste@gmail.com', 'senha' => 'abcd'),
     array('email' => 'userteste@gmail.com', 'senha' => '123456'),
 );
 
-foreach($usuarios_app as $user){
-    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
-        $usuario_autenticado = true;
-        
-    }
-    echo '<hr/>';
-}
-
-if($usuario_autenticado){ 
-    $_SESSION['autenticado'] = 'SIM';
-} else{
-    $_SESSION['autenticado'] = 'NÃO';
-    header('Location: index.php?login=erro');
-}
-
-// header('Location: home.php');
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
 
 ?>
