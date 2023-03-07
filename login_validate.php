2d280f8dce7f009188aff444739b7e5986a9ec04
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-03-07 20:39:29 -0300

    add user implementation

--- login_validate.php
+++ login_validate.php
@@ -4,21 +4,31 @@ session_start();
 print_r($_SESSION);
 
 $user_autenticated = false;
+$user_id = null;
+$user_id_perfil = null;
+
+$perfis = array(1 => 'admin', 2 => 'user');
 
 $usuarios_app = array(
-    array('email' => 'admteste@gmail.com', 'password' => 'abcd'),
-    array('email' => 'userteste@gmail.com', 'password' => '123456'),
+    array( 'id' => 1, 'email' => 'admteste@gmail.com', 'password' => 'abcd', 'perfil_id' => 1),
+    array( 'id' => 2,'email' => 'userteste@gmail.com', 'password' => '123456', 'perfil_id' => 1),
+    array( 'id' => 3,'email' => 'maria@teste.com', 'password' => '123456', 'perfil_id' => 2),
+    array( 'id' => 4,'email' => 'joao@teste.com', 'password' => '123456', 'perfil_id' => 2)
 );
 
 foreach($usuarios_app as $user){
     if($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']){
         $user_autenticated = true;
+        $user_id = $user['id'];
+        $user_id_perfil = $user['perfil_id'];
     }
 };
 
 if($user_autenticated){
     echo 'Usuario autenticado';
     $_SESSION['autenticado'] = true;
+    $_SESSION['id'] = $user_id;
+    $_SESSION['perfil_id'] = $user_id_perfil;
     header('Location: home.php?login=Succesfull');
 } else {
     $_SESSION['autenticado'] = false;
--- register_call.php
+++ register_call.php
@@ -1,10 +1,10 @@
 <?php 
-
+    session_start();
     $title = str_replace('#', '-', $_POST['title']);
     $category = str_replace('#', '-', $_POST['category']);
     $description = str_replace('#', '-', $_POST['description']);
     
-    $texto = $title . "#" . $category . "#" . $description . PHP_EOL;
+    $texto = $_SESSION['id'] . "#" . $title . "#" . $category . "#" . $description . PHP_EOL;
 
     $arquivo = fopen('archive.txt', 'a');
     fwrite($arquivo, $texto);
