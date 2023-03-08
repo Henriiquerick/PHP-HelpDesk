96ee55880b5b19490162de73c9b670cbb8290709
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-03-07 22:09:45 -0300

    adjusts in index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8" />
     <title>App Help Desk</title>
-
+    <link rel="icon" href="./images/iconfav2.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   </head>
