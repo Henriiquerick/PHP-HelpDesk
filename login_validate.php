dc742e5897c1c3848d792fa5fb65fd58f1d7c694
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-03-07 21:15:11 -0300

    add register page

--- login_validate.php
+++ login_validate.php
@@ -1,13 +1,15 @@
 <?php
 
 session_start();
-print_r($_SESSION);
 
 $user_autenticated = false;
 $user_id = null;
 $user_id_perfil = null;
 
 $perfis = array(1 => 'admin', 2 => 'user');
+
+print_r($_POST);
+
 
 $usuarios_app = array(
     array( 'id' => 1, 'email' => 'admteste@gmail.com', 'password' => 'abcd', 'perfil_id' => 1),
@@ -16,6 +18,24 @@ $usuarios_app = array(
     array( 'id' => 4,'email' => 'joao@teste.com', 'password' => '123456', 'perfil_id' => 2)
 );
 
+if($_POST['confirm_password'] != ''){
+    $_POST['id'] = 1;
+    $admin = 2;
+    if($_POST['perfil_id'] == 'on'){
+        $admin = 1;
+    }
+    foreach($usuarios_app as $user){
+        if($_POST['id'] == $user['id']){
+            $_POST['id'] += 1;
+        }
+    }
+    array_push($usuarios_app, array($_POST['id'], $_POST['email'], $_POST['password'], $admin));
+    echo "<pre>";
+    print_r($usuarios_app);
+    echo "</pre>";
+}else{
+    $_POST['confirm_password'] = $_POST['password'];
+}
 foreach($usuarios_app as $user){
     if($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']){
         $user_autenticated = true;
---
+++ register.php
@@ -0,0 +1,51 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="./images/logo.png" class="d-inline-block logo" alt="">
+        App Help Desk
+      </a>
+    </nav>
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Register
+            </div>
+            <div class="card-body">
+              <form action="login_validate.php" method="post">
+                <div class="form-group">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input name="password" type="password" class="form-control" placeholder="password">
+                </div>
+                <div class="form-group">
+                  <input name="confirm_password" type="password" class="form-control" placeholder="confirm password">
+                </div>
+                <div class="form-group">
+                  <input name="id" type="text" class="form-control" disabled>
+                </div>
+                <div class="form-group">
+                  <label for="perfil_id">Admin?</label>
+                  <input name="perfil_id" id="perfil_id" type="Checkbox" class="form-control">
+                </div>
+                <button class="btn btn-lg btn-info btn-block" type="submit">Register</button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
